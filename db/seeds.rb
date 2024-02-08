4cf1ca061fd19b712703451d182f9e32c764e846
BrvAlexis <brevieralexis@gmail.com>
2024-02-08 14:50:27 +0100

    finish

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,15 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Création de villes
+city1 = City.create(city_name: "Toulouse")
+city2 = City.create(city_name: "Paris")
+
+# Création de dogsitters
+dogsitter1 = Dogsitter.create(first_name: "John", last_name: "Doe", city: city1)
+dogsitter2 = Dogsitter.create(first_name: "Jane", last_name: "Smith", city: city2)
+
+# Création de chiens
+dog1 = Dog.create(name: "Buddy", city: city1)
+dog2 = Dog.create(name: "Charlie", city: city2)
